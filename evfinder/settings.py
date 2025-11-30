810f9bd431e5d93b8bfe6451156ca8636e5d9923
Sivakumar089 <kumarsiva200426@gamil.com>
2025-11-30 14:28:17 +0530

    smtp change .env

--- evfinder/settings.py
+++ evfinder/settings.py
@@ -132,12 +132,14 @@ REST_FRAMEWORK = {
     ),
 }
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'kumarsiva200426@gmail.com'
-EMAIL_HOST_PASSWORD = 'txtg ynja odij qxne'
+EMAIL_BACKEND = os.getenv('EMAIL_BACKEND')
+EMAIL_HOST = os.getenv('EMAIL_HOST')
+EMAIL_PORT = int(os.getenv('EMAIL_PORT'))
+EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS') == "True"
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
